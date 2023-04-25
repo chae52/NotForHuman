9e689a466cf321410a9deccbafb913ff513d6e1b
chae52 <fjdksla98@naver.com>
2023-04-25 22:51:29 +0900

    상단

--- src/pages/animals.jsx
+++ src/pages/animals.jsx
@@ -2,9 +2,18 @@ import Header from "../components/Header";
 
 const Animals = () =>{
     return (
-        <div>
+        <div id="">
             <Header/>
-            test Animals
+            
+            <span class='background'>
+            </span>
+            <div class="profile_container">
+                <div class="profile">
+                </div>
+            </div>
+                
+            <h1>ANIMALS</h1>
+            <button id='b'>ToPlant</button>
         </div>
     );
 }
