9dae166f2144b8d2989dcc67956f04f1a7599aeb
DESKTOP-LTULUJU\ASUS <chj990707@postech.ac.kr>
2023-04-25 22:43:06 +0900

    detail page: basic layout

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,11 +1,63 @@
 import Header from "../components/Header";
+import { useParams } from 'react-router-dom';
+import "./detail.css"
 
-const Detail = () =>{
+const Detail = () => {
+
+    let post = [
+        {
+            id: 1,
+            title: "Hello",
+            writer: "Arthur",
+            image : "a.jpg",
+            contents: "Lorem"
+        },
+        {
+            id: 2,
+            title: "world!",
+            writer: "Dent",
+            image: "b.jfif",
+            contents: "Ipsum"
+        }
+    ];
+
+    let { id } = useParams();
+
     return (
-        <div>
-            <Header/>
-            test Detail
-        </div>
+        <html>
+            <head>
+                <meta charset="UTF-8" />
+            </head>
+            <body>
+                <Header />
+                <div className="cover">
+
+                    <span className="Title">
+                        <p>
+                            <a>제목 { post[id].title }</a>
+                        </p>
+                    </span>
+    
+                    <span className="Writer">
+                        <p>
+                            <a>작성자 {post[id].writer}</a>
+                        </p>
+                    </span>
+                </div>
+                <div className="main">
+                    <span className="image">
+                        <img src={require("./img/" + post[id].image)} />
+                    </span>
+
+                    <span className="Contents">
+                        <p>
+                            <a>내용 { post[id].contents }</a>
+                        </p>
+                    </span>
+                </div>
+            </body>
+        </html>
+
     );
 }
 
